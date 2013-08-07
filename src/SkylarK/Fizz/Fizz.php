1893f4fb3c2c4b667cd96242d98a2d3e50c3f09e
James Thompson <keiouu@gmail.com>
2013-08-07 20:51:18 +0100

    Var tracing

--- src/SkylarK/Fizz/Fizz.php
+++ src/SkylarK/Fizz/Fizz.php
@@ -8,8 +8,35 @@
 namespace SkylarK\Fizz;
 
 /**
- * The core of Fizz
+ * The core of Fizz, all models should extend this
  */
-class Fizz
+abstract class Fizz
 {
+	/** Our PDO instance */
+	private $_fizz_pdo;
+	/** The name of the table we use */
+	private $_fizz_table;
+	/** A list of fields we have */
+	private $_fizz_fields;
+
+	/**
+	 * Initialize Fizz with valid database connection details
+	 * 
+	 * @param string $db_string A PDO connection string
+	 * @param string $table The name of the table this model relates too
+	 */
+	public function __construct($db_dsn, $db_username = NULL, $db_password = NULL, $table = NULL) {
+		$this->_fizz_pdo = new \PDO($db_dsn, $db_username, $db_password);
+		$this->_fizz_table = empty($table) ? get_called_class() : $table;
+
+		// Extract the fields
+		$this->_fizz_fields = get_object_vars($this);
+	}
+
+	/**
+	 * Return a list of fields we know about.
+	 */
+	public function fields() {
+		return $this->_fizz_fields;
+	}
 }
--- tests/SkylarK/Fizz/FizzTest.php
+++ tests/SkylarK/Fizz/FizzTest.php
@@ -1,9 +1,36 @@
 <?php
+
+class Demo extends SkylarK\Fizz\Fizz
+{
+	public $key;
+	public $value;
+}
+
 class FizzTest extends PHPUnit_Framework_TestCase
 {
-   private $_fizz;
+	private static $_fizz;
 
-   protected function setUp() {
-      $this->_fizz = new SkylarK\Fizz\Fizz();
-   }
+	public static function setUpBeforeClass() {
+		try {
+			self::$_fizz = new Demo("mysql:dbname=testdb;host=127.0.0.1", "travis", "");
+		}
+		catch (PDOException $e) {
+			die($e->getMessage());
+			exit(0);
+		}
+	}
+
+	public static function tearDownAfterClass() {
+		self::$_fizz = NULL;
+	}
+
+	// Tests
+
+	public function test_Grab_Vars() {
+		$this->assertEqual(array("key", "value"), self::$_fizz->fields());
+	}
+
+	//public function test_Table_Install() {
+		//$this->assertTrue(self::$_fizz->install());
+	//}
 }
