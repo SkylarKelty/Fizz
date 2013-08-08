a55924ae36a093d28fa8e2565ec7602b8a0003d6
Skylar Kelty <skylarkelty@gmail.com>
2013-08-08 08:11:54 +0100

    Begin migrations work

---
+++ src/SkylarK/Fizz/Util/FizzMigrate.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Fizz is a lightweight ORM
+ *
+ * @author Skylar Kelty <skylarkelty@gmail.com>
+ */
+
+namespace SkylarK\Fizz;
+
+/**
+ * The core of Fizz, all models should extend this
+ */
+abstract class Fizz
+{
+}
