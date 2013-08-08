c7831a33982dd3ec88047cae0d4d8cf615a9280b
Skylar Kelty <skylarkelty@gmail.com>
2013-08-08 08:12:36 +0100

    Rename that file

--- src/SkylarK/Fizz/Util/FizzMigrate.php
+++ src/SkylarK/Fizz/Util/FizzMigrate.php
@@ -5,11 +5,58 @@
  * @author Skylar Kelty <skylarkelty@gmail.com>
  */
 
-namespace SkylarK\Fizz;
+namespace SkylarK\Fizz\Util;
 
 /**
- * The core of Fizz, all models should extend this
+ * FizzMigrate can be used to handle database schemas
  */
-abstract class Fizz
+class FizzMigrate
 {
+	/** Store our table name */
+	private $_table;
+	/** Track our version */
+	private $_version;
+
+	/**
+	 * Construct a new migrations object.
+	 *
+	 * A migration takes the following form:
+	 *
+	 * // First, create the start object
+	 * $object = new FizzMigrate("MetaData");
+	 * $object->addField("key", "varchar", 250, true, true);
+	 * $object->addField("value", "varchar", 250, true, false);
+	 *
+	 * // Then list your changes
+	 * $object->beginMigration();
+	 * $object->retype("value", "text");
+	 * $object->resize("value", NULL);
+	 * $object->endMigration();
+	 *
+	 * // More changes later
+	 * $object->beginMigration();
+	 * $object->removePrimaryKey("value");
+	 * $object->endMigration();
+	 * 
+	 * @param string $tableName The name of the table we relate too
+	 * @param string $schemaVersion The version of the schema we are working on. Important for upgrades
+	 */
+	public function __construct($tableName) {
+		$this->_table = $tableName;
+		$this->_version = 0;
+	}
+
+	/**
+	 * Begin a new set of migrations
+	 */
+	public function beginMigration() {
+		$this->_version++;
+	}
+
+	/**
+	 * End a set of migrations
+	 */
+	public function beginMigration() {
+		// -
+	}
 }
