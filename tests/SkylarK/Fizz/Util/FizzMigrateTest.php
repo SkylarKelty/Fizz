94a0e17a06715ba21f72c9d904cdb584ebad4f5b
Skylar Kelty <skylarkelty@gmail.com>
2013-08-09 20:58:41 +0100

    Add migrate tests

---
+++ tests/SkylarK/Fizz/Util/FizzMigrateTest.php
@@ -0,0 +1,28 @@
+<?php
+class FizzMigrateTest extends PHPUnit_Framework_TestCase
+{
+	public static function setUpBeforeClass() {
+		try {
+			SkylarK\Fizz\FizzConfig::setDB("mysql:dbname=testdb;host=127.0.0.1", "travis", "");
+		}
+		catch (PDOException $e) {
+			die($e->getMessage());
+			exit(0);
+		}
+	}
+
+	// Tests
+
+	public function test_CommitFailsWithoutFields() {
+		$object = new SkylarK\Fizz\Util\FizzMigrate("Example");
+		$this->assertFalse($object->commit());
+	}
+
+	public function test_Commit() {
+		$object = new SkylarK\Fizz\Util\FizzMigrate("Example");
+		$object->addField("key", "int(11)");
+		$object->addField("value", "varchar(125)");
+		$object->setPrimary("key", true);
+		$this->assertTrue($object->commit());
+	}
+}
